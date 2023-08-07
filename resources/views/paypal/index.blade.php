d6b23e3d143d4f25e757da7282c3ca49fd2517c8
chanz-pclimaco <pclimaco@chanzglobal.com>
2023-08-07 10:25:34 +0800

    Initial Commit

---
+++ resources/views/paypal/index.blade.php
@@ -0,0 +1,46 @@
+<!Doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  
+    <title>{{ config('app.name', 'Laravel') }}</title>
+  
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+  
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  
+    <style type="text/css">
+        i{
+            font-size: 50px;
+        }
+    </style>
+</head>
+<body>
+    <div id="app">
+        <main class="container">
+            <div class="card mt-5">
+              <div class="card-header">
+                <h1 class="text-3xl md:text-5xl font-extrabold text-center uppercase mb-12 bg-gradient-to-r from-indigo-400 via-purple-500 to-indigo-600 bg-clip-text text-transparent transform -rotate-2">Make A Payment</h1>
+              </div>
+              <div class="card-body text-center">
+                    @if (session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session()->get('success') }}
+                        </div>
+                    @endif
+                    <center>
+                        <a href="{{ route('make.payment') }}" class="w-full bg-indigo-500 uppercase rounded-xl font-extrabold text-black px-6 h-8">Pay with PayPal👉</a>
+                    </center>
+              </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
